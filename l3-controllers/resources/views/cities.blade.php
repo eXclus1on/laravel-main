44aad85f89e53ab40f3dc61cafa6a1f226b71fe6
Rodolfo Escaleira <74121364+eXclus1on@users.noreply.github.com>
2024-04-02 21:28:28 +0100

    Add external Pico CSS library to cities.blade.php and homepage.blade.php
    The diff adds the external Pico CSS library to the head section of cities.blade.php and homepage.blade.php. The previous internal css/pico.min.css is replaced with the external version from jsdelivr.net. Additionally, a search form is added to cities.blade.php to filter city results.

--- l3-controllers/resources/views/cities.blade.php
+++ l3-controllers/resources/views/cities.blade.php
@@ -3,10 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+/>
     <title>Aula 02-04-2024</title>
 </head>
 <body>
     <h1>City List</h1>
+    <form action="/cities" method="get">
+        <input type="text" placeholder="Search city..." name="city"/>
+        <button type="submit">Filter</button>
+    </form>
     
 </body>
 </html>
--- l3-controllers/resources/views/homepage.blade.php
+++ l3-controllers/resources/views/homepage.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/pico.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"/>
     <title>Aula 02-04-2024</title>
 </head>
 
