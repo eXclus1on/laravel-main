ad842e9695bfddc827bd79da144f6e2308e41af3
Rodolfo Escaleira <74121364+eXclus1on@users.noreply.github.com>
2024-04-06 12:43:07 +0100

    Title: Updated title tag in paginatedCities view
    Body:
    - Changed the title tag from "Document" to "Aula 04-04-2024" in the `paginatedCities.blade.php` file.

--- l3-controllers/resources/views/paginatedCities.blade.php
+++ l3-controllers/resources/views/paginatedCities.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Aula 04-04-2024</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
 </head>
 
