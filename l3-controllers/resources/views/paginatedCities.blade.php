3f2ddd0cbc7b0c04c0a241eaf8f02a454b924ef6
Rodolfo Escaleira <74121364+eXclus1on@users.noreply.github.com>
2024-04-11 17:30:39 +0100

    Add Pico CSS to l3-controllers

--- l3-controllers/resources/views/paginatedCities.blade.php
+++ l3-controllers/resources/views/paginatedCities.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"/>
+    <title>Controllers - Aula laravel 09-04-2024</title>
 </head>
 
 <body>
